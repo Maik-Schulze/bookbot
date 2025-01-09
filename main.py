3d61ff9cbb7c15da386bf8456401dc83ed3dd419
Maik-Schulze <maik.schulze99@gmail.com>
2025-01-09 15:54:34 +0100

    add function to read file and cleaned up main function

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def main():
     path = "books/frankenstein.txt"
+    
+    text = read_file(path)
+
+    word_count = get_num_words(text)
+    char_dict = get_character_count(text)
+
+    for char in char_dict:
+        print(char_dict)
+
+def read_file(path):
     with open(path) as f:
         text = f.read()
-    char_dict = get_character_count(text)
-    for char in char_dict:
-        print(f"{char}: {char_dict[char]}")
+    return text
 
 def get_num_words(text):
     return len(text.split())
