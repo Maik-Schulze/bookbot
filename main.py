a0beb4562021bacccba9a372f382ac375963fe26
Maik-Schulze <maik.schulze99@gmail.com>
2025-01-09 16:10:04 +0100

    add character report functionality

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def main():
     path = "books/frankenstein.txt"
-    
     text = read_file(path)
-
+    
     word_count = get_num_words(text)
     char_dict = get_character_count(text)
+    characters = get_character_report(char_dict)
 
-    for char in char_dict:
-        print(char_dict)
+    print_character_report(characters, path, word_count)
+
 
 def read_file(path):
     with open(path) as f:
@@ -26,5 +26,23 @@ def get_character_count(text):
         else:
             char_dict[lowered] = 1
     return char_dict
+
+def sort_on(dict):
+    return dict["count"]
+
+def get_character_report(dict):
+    characters = []
+    for char in dict:
+        if char.isalpha():
+            characters.append({"char": char, "count": dict[char]})
+    characters.sort(reverse=True, key=sort_on)
+    return characters
+
+def print_character_report(characters, path, word_count):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document\n")
+    for c in characters:
+        print(f"The \'{c["char"]}\' character was found {c["count"]} times")
+    print("--- End report ---")
 
 main()
